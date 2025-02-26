712460e295dfa24af82ec4f449645d44068da2d9
dprandle <dprandle@alaska.edu>
2025-02-25 19:46:46 -0900

    Added in basic serving capabilities

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import path from "path";
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname,'../public')));
+
+// This request isnt needed because the index.html is already being served by the express.static middleware
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 app.listen(port, () => {
