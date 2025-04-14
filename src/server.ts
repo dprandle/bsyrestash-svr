3768346b988e52dcdf1a3336104b976590355055
dprandle <daniel@zetrickllc.com>
2025-04-13 21:55:17 -0500

    Update

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import path from "path";
-const app = express();
+const app = express();;
 const port = 3000;
 
 app.use(express.static(path.join(__dirname,'../public')));
 
 // This request isnt needed because the index.html is already being served by the express.static middleware
-app.get("/", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
